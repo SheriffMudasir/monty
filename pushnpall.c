539871fde04db453e2823c8870755fc76a02afaf
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-10-21 15:35:53 -0700

    betty

--- pushnpall.c
+++ pushnpall.c
@@ -9,42 +9,39 @@
 
 int main(int argc, char **argv)
 {
-    char *op;
-    char *arg;
-    stack_t *stack = NULL;
-    FILE *file = fopen(argv[1], "r");
-    char *input;
-    size_t len;
-    unsigned int line_number;
+	char *op;
+	char *arg;
+	stack_t *stack = NULL;
+	FILE *file = fopen(argv[1], "r");
+	char *input;
+	size_t len;
+	unsigned int line_number;
+	
+	
+	stack = NULL;
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	
+	input = NULL;
+	len = 0;
+	line_number = 0;
 
-
-
-    stack = NULL;
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-
-    input = NULL;
-    len = 0;
-    line_number = 0;
-
-
-    #define _GNU_SOURCE
-    while (getline(&input, &len, file) != -1)
-    {
-        line_number++;
-
-        op = strtok(input, " \t\n");
-        arg = strtok(NULL, " \t\n");
-
-        if (op == NULL)
-            continue;
-
-        if (strcmp(op, "push") == 0)
-        {
+#define _GNU_SOURCE
+	while (getline(&input, &len, file) != -1)
+	{
+		line_number++;
+		op = strtok(input, " \t\n");
+		arg = strtok(NULL, " \t\n");
+		
+		if (op == NULL)
+			continue;
+		
+		if (strcmp(op, "push") == 0)
+		{
             if (arg == NULL)
             {
                 fprintf(stderr, "L%u: usage: push integer\n", line_number);
