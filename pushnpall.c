543f32c55363c521a23e2b66e893d826b04ceeb1
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-10-21 15:19:59 -0700

    Furnishing

--- pushnpall.c
+++ pushnpall.c
@@ -9,25 +9,28 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-
     char *op;
     char *arg;
     stack_t *stack = NULL;
     FILE *file = fopen(argv[1], "r");
+    char *input;
+    size_t len;
+    unsigned int line_number;
+
+
+
+    stack = NULL;
+    file = fopen(argv[1], "r");
     if (file == NULL)
     {
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
-    char *input = NULL;
-    size_t len = 0;
-    unsigned int line_number = 0;
+    input = NULL;
+    len = 0;
+    line_number = 0;
+
 
     #define _GNU_SOURCE
     while (getline(&input, &len, file) != -1)
