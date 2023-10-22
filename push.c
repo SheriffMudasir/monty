555e32fb3b4a371d0d983444f9b657f595f13121
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-10-22 08:22:20 -0700

    Finishing

--- push.c
+++ push.c
@@ -8,18 +8,19 @@
 
 void push(stack_t **stack, int value)
 {
-        stack_t *new_node = malloc(sizeof(stack_t));
-        if (new_node == NULL)
-        {
-                fprintf(stderr, "Error: malloc failed\n");
-                exit(EXIT_FAILURE);
-        }
+	stack_t *new_node = malloc(sizeof(stack_t));
 
-        new_node->n = value;
-        new_node->prev = NULL;
-        new_node->next = *stack;
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-        if (*stack)
-                (*stack)->prev = new_node;
-        *stack = new_node;
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = new_node;
+	*stack = new_node;
 }
